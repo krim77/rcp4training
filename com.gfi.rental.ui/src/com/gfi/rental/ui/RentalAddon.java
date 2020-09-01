9daad12785d4f786c8a4feb37f98f1ae0e5af311
krim belhocine <krimbel2000@yahoo.fr>
2020-09-01 11:06:41 +0200

    Ajout gestion des images

--- com.gfi.rental.ui/src/com/gfi/rental/ui/RentalAddon.java
+++ com.gfi.rental.ui/src/com/gfi/rental/ui/RentalAddon.java
@@ -8,12 +8,16 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.EventTopic;
 import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.event.Event;
 
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.helpers.RentalAgencyGenerator;
 
-public class RentalAddon {
+public class RentalAddon implements RentalUIConstants{
 
 	@Inject
 	@Optional
@@ -28,6 +32,29 @@ public class RentalAddon {
 	{
 		System.out.println("On passe dans l'Addon");
 		iec.set(RentalAgency.class, RentalAgencyGenerator.createSampleAgency());
+		iec.set(RENTAL_UI_IMG_REGISTRY, getLocalImageRegistry());
 		
+	}
+	/**
+	 * A method to create and initialize an ImageRegistry
+	 * @return a initialized ImageRegistry that can be put in the context
+	 */
+	ImageRegistry getLocalImageRegistry()
+	{
+		// Get the bundle using the universal method to get it from the current class
+		Bundle b = FrameworkUtil.getBundle(getClass());  
+		
+		// Create a local image registry
+		ImageRegistry reg = new ImageRegistry();
+
+		// Then fill the values...
+		reg.put(IMG_CUSTOMER, ImageDescriptor.createFromURL(b.getEntry(IMG_CUSTOMER)));
+		reg.put(IMG_RENTAL, ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL)));
+		reg.put(IMG_RENTAL_OBJECT, ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL_OBJECT)));
+		reg.put(IMG_AGENCY, ImageDescriptor.createFromURL(b.getEntry(IMG_AGENCY)));
+		reg.put(IMG_COLLAPSE_ALL, ImageDescriptor.createFromURL(b.getEntry(IMG_COLLAPSE_ALL)));
+		reg.put(IMG_EXPAND_ALL, ImageDescriptor.createFromURL(b.getEntry(IMG_EXPAND_ALL)));
+
+		return reg;
 	}
 }
